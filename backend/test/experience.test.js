36cab2e030fa21774495f1be6a33d8f6c9ebd7e8
Jtrep10 <jtrep10@gmail.com>
2024-11-25 16:49:39 +0000

    extended backend tests

--- backend/test/experience.test.js
+++ backend/test/experience.test.js
@@ -5,6 +5,9 @@ const { modifyExperience } = require("../controllers/api/v1/profile_apis"); // U
 const Education = require("../models/education");
 const Experience = require("../models/experience");
 const request = require('supertest');
+const mockingoose = require("mockingoose");
+const User = require("../models/user");
+
 require("dotenv").config();
 jest.mock("../models/education");
 jest.mock("../models/experience");
@@ -78,5 +81,45 @@ describe('POST /createExperience', () => {
 
   });
 
+  it('should successfully retrieve all experiences for an applicant', async () => {
+    // Mocking the result of the Experience.find() method
+    const mockExperiences = [
+      {
+        _id: '60d0fe4f5311236168a109f9',
+        applicantId: '60d0fe4f5311236168a109f8',
+        title: 'Software Engineer',
+        company: 'Example Corp',
+        fromDate: new Date('2022-01-01'),
+        toDate: new Date('2023-01-01'),
+      },
+      {
+        _id: '60d0fe4f5311236168a109fa',
+        applicantId: '60d0fe4f5311236168a109f8',
+        title: 'Junior Developer',
+        company: 'Tech Startup',
+        fromDate: new Date('2020-01-01'),
+        toDate: new Date('2021-01-01'),
+      }
+    ];
 
+    // Mock the Experience.find() function to return the mock experiences
+    Experience.find.mockResolvedValue(mockExperiences);
+    // mockingoose(Experience).toReturn(mockExperiences, 'find');
+
+    const reqBody = { params: {applicantId: '60d0fe4f5311236168a109f7'}};
+
+    // Call the GET endpoint
+    // Call the create function
+    await getAllExperience(reqBody, res);
+
+    // Assert the response structure
+    //expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith({
+            message: "Experience retrieved Successfuly",
+            data: {
+              'experience': "60d0fe4f5311236168a109f9",
+            },
+            success: true,
+    });
+});
 });
