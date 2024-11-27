5e2d37731f7fbf5376037fa31989808a66a83154
Jtrep10 <jtrep10@gmail.com>
2024-11-27 02:48:35 +0000

    generalized mongoose

--- backend/config/mongoose.js
+++ backend/config/mongoose.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
 
-connection_string='mongodb+srv://mulesiddhi:mulesiddhi27@cluster0.et5q4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-//mongoose.connect('mongodb://localhost/wolfjobs_development');
+connection_string = 'mongodb://localhost/wolfjobs_development';
 mongoose.connect(connection_string)
 const db = mongoose.connection;
 
